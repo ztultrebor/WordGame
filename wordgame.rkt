652a380a9b5218e49d71d746eea9e4a37c3d9452
Robert Lutz <robertlutzi@gmail.com>
2023-12-26 15:37:50 -0500

    improved refactoring for optimal use of local expressions

--- wordgame.rkt
+++ wordgame.rkt
@@ -46,54 +46,6 @@
   ; String -> [ListOf String]
   ; generate a list of all valid anagrams of the given word
   (local (
-          (define (permute lo1s0 lo1s1 lo1s2)
-            ; [ListOf 1String] [ListOf 1String] [ListOf 1String]
-            ; -> [ListOf ListOf 1String]
-            ; generate all unique permutations of elements of a given list
-            (cond
-              [(empty? lo1s2) lo1s0]
-              [(empty? lo1s1) '()]
-              [else
-               (local (
-                       (define (smart-merge l1 l2)
-                         ; [ListOf ListOf 1String] [ListOf ListOf 1String]
-                         ; -> [ListOf ListOf 1String]
-                         ; decides when to create a set from a permuted
-                         ; list, and when to cons it
-                         (cond
-                           [(cons? (first l1)) (create-set l1 l2)]
-                           [else (cons l1 l2)]))
-                       (define (pull-from-set ele lst)
-                         ; 1String [ListOf 1String] -> [ListOf 1String]
-                         ; removes an element to a set if its in there
-                         ; assumes the list provided qualifies as a set
-                         (cond
-                           [(empty? lst) '()]
-                           [(equal? ele (first lst)) (rest lst)]
-                           [else (cons (first lst)
-                                       (pull-from-set ele (rest lst)))]))
-                       (define reduced-set
-                         (pull-from-set (first lo1s1) lo1s2))
-                       (define permute-first
-                         (permute(cons (first lo1s1) lo1s0)
-                                 reduced-set reduced-set))
-                       (define permute-rest (permute lo1s0 (rest lo1s1) lo1s2)))
-                 (smart-merge permute-first permute-rest))]))
-          (define (recombine lolo1s)
-            ; [ListOf ListOf 1String] -> [ListOf String]
-            ; collapse each [ListOf 1String] into a String
-            (cond
-              [(empty? lolo1s) '()]
-              [else (cons (implode (first lolo1s)) (recombine (rest lolo1s)))]))
-          (define (permutations-to-words los)
-            ; [ListOf String] [ListOf String] -> [ListOf String]
-            ; purge all nonsense permutations from a list.
-            ; Real words are contained in the global dictionary indicated
-            (cond
-              [(empty? los) '()]
-              [(member? (first los) DICTIONARY)
-               (cons (first los) (permutations-to-words (rest los)))]
-              [else (permutations-to-words (rest los))]))
           (define exploded (explode word))
           (define raw-permutations (permute '() exploded exploded))
           (define permutations (recombine raw-permutations)))
@@ -106,6 +58,56 @@
 (check-satisfied (list (get-anagrams "fxq") '()) same-set?)
 
 
+(define (permute lo1s0 lo1s1 lo1s2)
+  ; [ListOf 1String] [ListOf 1String] [ListOf 1String]
+  ; -> [ListOf ListOf 1String]
+  ; generate all unique permutations of elements of a given list
+  (cond
+    [(empty? lo1s2) lo1s0]
+    [(empty? lo1s1) '()]
+    [else
+     (local (
+             (define (pull-from-set ele lst)
+               ; 1String [ListOf 1String] -> [ListOf 1String]
+               ; removes an element to a set if its in there
+               (cond
+                 [(equal? ele (first lst)) (rest lst)]
+                 [else (cons (first lst)
+                             (pull-from-set ele (rest lst)))]))
+             (define (smart-merge l1 l2)
+               ; [ListOf ListOf 1String] [ListOf ListOf 1String]
+               ; -> [ListOf ListOf 1String]
+               ; decides when to create a set from two permuted
+               ; lists, and when to cons them
+               (cond
+                 [(cons? (first l1)) (create-set l1 l2)]
+                 [else (cons l1 l2)]))
+             (define reduced-set (pull-from-set (first lo1s1) lo1s2))
+             (define permute-first
+               (permute (cons (first lo1s1) lo1s0) reduced-set reduced-set))
+             (define permute-rest (permute lo1s0 (rest lo1s1) lo1s2)))
+       (smart-merge permute-first permute-rest))]))
+
+
+(define (recombine lolo1s)
+  ; [ListOf ListOf 1String] -> [ListOf String]
+  ; collapse each [ListOf 1String] into a String
+  (cond
+    [(empty? lolo1s) '()]
+    [else (cons (implode (first lolo1s)) (recombine (rest lolo1s)))]))
+
+
+(define (permutations-to-words los)
+  ; [ListOf String] [ListOf String] -> [ListOf String]
+  ; purge all nonsense permutations from a list.
+  ; Real words are contained in the global dictionary indicated
+  (cond
+    [(empty? los) '()]
+    [(member? (first los) DICTIONARY)
+     (cons (first los) (permutations-to-words (rest los)))]
+    [else (permutations-to-words (rest los))]))
+
+          
 (define (create-set lst1 lst2)
   ; ListOfAny ListOfAny -> ListOfAny
   ; merges two lists into a set; a list in which no items repeat.
