5caeae52392b512496293b087863850a3e35e95f
Robert Lutz <robertlutzi@gmail.com>
2023-12-30 13:36:08 -0500

    refactor using match where sensible

--- wordgame.rkt
+++ wordgame.rkt
@@ -6,9 +6,9 @@
 
 ; data definitions
 
-; A ListOf1Strings is one of:
+; A [ListOf 1String] is one of:
 ;     - '()
-;     - (cons 1String ListOf1Strings)
+;     - (cons 1String [ListOf 1String])
 #;
 (define (fn-on-lo1s lo1s)
   (cond
@@ -19,9 +19,9 @@
           ... (fn-on-lo1s (rest lo1s))]))
 
 
-; A ListOfListOf1Strings is one of:
+; A [ListOf [ListOf 1String]] is one of:
 ;     - '()
-;     - (cons ListOfStrings ListOfListOf1Strings)
+;     - (cons [ListOf 1String] [ListOf [ListOf 1String]])
 #;
 (define (fn-on-lolo1s lolo1s)
   (cond
@@ -47,8 +47,8 @@
   ; generate a list of all valid anagrams of the given word
   (local (
           (define exploded (explode word))
-          (define non-unique-permutations (permute (list '() exploded)))
-          (define permutations (list-to-set non-unique-permutations)))
+          (define indistinct-permutations (permute (list '() exploded)))
+          (define permutations (list-to-set indistinct-permutations)))
     ; - IN -
     (permutations-to-words permutations)))
 ; checks
@@ -66,62 +66,13 @@
   ; This function is auxiliary to get-anagrams
   ; gets all permutations of a given word that has been exploded and parsed
   ; permutations are not guaranteed to be unique
-    (cond
-      [(empty? (second candi-fractures)) (list (implode (first candi-fractures)))]
-      [else
-       (foldr append '()
-              (map permute
-                   (map (lambda (c)
-                          (list (cons c (first candi-fractures))
-                                (remove c (second candi-fractures))))
-                        (second candi-fractures))))]))
-
-
-#;
-(define (permute lo1s0 lo1s1 lo1s2)
-  ; [ListOf 1String] [ListOf 1String] [ListOf 1String]
-  ; -> [ListOf ListOf 1String]
-  ; hard core version, for serious computer scientologists only
-  ; built from base metal
-  (cond
-    [(empty? lo1s2) lo1s0]
-    [(empty? lo1s1) '()]
-    [else
-     (local (
-             (define (pull-from-set ele lst)
-               ; 1String [ListOf 1String] -> [ListOf 1String]
-               ; removes an element to a set if its in there
-               (cond
-                 [(equal? ele (first lst)) (rest lst)]
-                 [else (cons (first lst)
-                             (pull-from-set ele (rest lst)))]))
-             (define suffix (pull-from-set (first lo1s1) lo1s2))
-             ; suffix is the remaining letters of the complete permutation
-             (define permute-first
-               (permute (cons (first lo1s1) lo1s0) suffix suffix))
-             (define permute-rest (permute lo1s0 (rest lo1s1) lo1s2))
-             (define (smart-merge l1 l2)
-               ; [ListOf ListOf 1String] [ListOf ListOf 1String]
-               ; -> [ListOf ListOf 1String]
-               ; decides when to create a set from two permuted
-               ; lists, and when to cons them
-               (cond
-                 [(cons? (first l1)) (create-set l1 l2)]
-                 [else (cons l1 l2)])))
-       ; - IN -
-       (smart-merge permute-first permute-rest))]))
-
-
-(define (permutations-to-words los)
-  ; [ListOf String] [ListOf String] -> [ListOf String]
-  ; purge all nonsense permutations from a list.
-  ; Real words are contained in the global dictionary indicated
-  (cond
-    [(empty? los) '()]
-    [(member? (first los) DICTIONARY)
-     (cons (first los) (permutations-to-words (rest los)))]
-    [else (permutations-to-words (rest los))]))
-
+  (match candi-fractures
+    [(list prefix '()) (list (implode prefix))]
+    [(list prefix suffix)
+     (foldr append '()
+            (map permute
+                 (map (lambda (c) (list (cons c prefix) (remove c suffix)))
+                      suffix)))]))
 
 
 (define (list-to-set lst)
@@ -139,14 +90,15 @@
     (setterator '() lst)))
 
 
-(define (push-to-set ele lst)
-  ; X [ListOf X] -> [ListOf X]
-  ; adds an element to a set--a list in which no items repeat.
-  ; assumes the list provided qualifies as a set
+(define (permutations-to-words los)
+  ; [ListOf String] [ListOf String] -> [ListOf String]
+  ; purge all nonsense permutations from a list.
+  ; Real words are contained in the global dictionary indicated
   (cond
-    [(empty? lst) (list ele)]
-    [(equal? ele (first lst)) lst]
-    [else (cons (first lst) (push-to-set ele (rest lst)))]))
+    [(empty? los) '()]
+    [(member? (first los) DICTIONARY)
+     (cons (first los) (permutations-to-words (rest los)))]
+    [else (permutations-to-words (rest los))]))
 
 
 (define (same-set? lsets)
@@ -161,9 +113,21 @@
   ; [ListOf X] [ListOf X] -> [ListOf X]
   ; merges two lists into a set; a list in which no items repeat.
   ; assumes both lists provided qualify as sets
-  (cond
-    [(empty? lst1) lst2]
-    [else (push-to-set (first lst1) (create-set (rest lst1) lst2))]))
+  (match lst1
+    ['() lst2]
+    [(cons pre post) (push-to-set pre (create-set post lst2))]))
+
+
+(define (push-to-set ele lst)
+  ; X [ListOf X] -> [ListOf X]
+  ; adds an element to a set--a list in which no items repeat.
+  ; assumes the list provided qualifies as a set
+  (match lst
+    ['() (list ele)]
+    [(cons ele post) lst]
+    [(cons pre post) (cons pre (push-to-set ele post))]))
+
+
 
 ; actions!
 
